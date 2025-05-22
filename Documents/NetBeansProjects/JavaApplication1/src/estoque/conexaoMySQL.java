131cf86ec954802af9254b40a3561a0e1b80d371
Rafudoo <uaf.a176@gmail.com>
2025-05-21 22:06:39 -0300

    Primeiro commit do projeto java

---
+++ Documents/NetBeansProjects/JavaApplication1/src/estoque/conexaoMySQL.java
@@ -0,0 +1,14 @@
+package estoque;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conexaoMySQL {
+    public static Connection conectar() throws Exception {
+        String url = "jdbc:mysql://localhost:3306/estoque";
+        String usuario = "root";
+        String senha = "fc1fab63";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, usuario, senha);
+    }
+}
